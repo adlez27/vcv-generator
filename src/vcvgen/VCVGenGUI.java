a07039a117c07d25fce98e6bb2de47bd3b336b9b
Kiyoteru Lofe Adlez Dystopia <meggiebeltran@gmail.com>
2017-05-12 21:46:48 -0400

    Add files via upload

---
+++ src/vcvgen/VCVGenGUI.java
@@ -0,0 +1,204 @@
+package vcvgen;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class VCVGenGUI implements ActionListener{
+	
+	private static JFrame frame;
+	private static JTextField txtConsonants;
+	private static JTextField txtVowels;
+	private static JTextField txtMaxsyl;
+	private static JTextField txtInitoff;
+	private static JTextField txtTempo;
+	private static JTextField txtMaxdups;
+	private static JButton btnGenerate;
+	
+	public static void main(String[] args){
+		VCVGenGUI nya = new VCVGenGUI();
+		nya.GWEEE();
+	}
+	
+	public void GWEEE(){
+		frame = new JFrame("VCV Generator");
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{235, 0};
+		gridBagLayout.rowHeights = new int[]{25, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		
+		JPanel panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel.anchor = GridBagConstraints.NORTH;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
+		frame.getContentPane().add(panel, gbc_panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0, 0};
+		gbl_panel.rowHeights = new int[]{15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblReclist = new JLabel("Reclist");
+		GridBagConstraints gbc_lblReclist = new GridBagConstraints();
+		gbc_lblReclist.gridwidth = 2;
+		gbc_lblReclist.anchor = GridBagConstraints.NORTH;
+		gbc_lblReclist.insets = new Insets(0, 0, 5, 0);
+		gbc_lblReclist.gridx = 0;
+		gbc_lblReclist.gridy = 0;
+		panel.add(lblReclist, gbc_lblReclist);
+		
+		JLabel lblConsonants = new JLabel("Consonants");
+		GridBagConstraints gbc_lblConsonants = new GridBagConstraints();
+		gbc_lblConsonants.anchor = GridBagConstraints.WEST;
+		gbc_lblConsonants.gridwidth = 2;
+		gbc_lblConsonants.insets = new Insets(0, 0, 5, 0);
+		gbc_lblConsonants.gridx = 0;
+		gbc_lblConsonants.gridy = 1;
+		panel.add(lblConsonants, gbc_lblConsonants);
+		
+		txtConsonants = new JTextField();
+		txtConsonants.setText("separated by spaces");
+		GridBagConstraints gbc_txtConsonants_1 = new GridBagConstraints();
+		gbc_txtConsonants_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtConsonants_1.gridwidth = 2;
+		gbc_txtConsonants_1.insets = new Insets(0, 0, 5, 0);
+		gbc_txtConsonants_1.gridx = 0;
+		gbc_txtConsonants_1.gridy = 2;
+		panel.add(txtConsonants, gbc_txtConsonants_1);
+		txtConsonants.setColumns(10);
+		
+		JLabel lblVowels = new JLabel("Vowels");
+		GridBagConstraints gbc_lblVowels = new GridBagConstraints();
+		gbc_lblVowels.anchor = GridBagConstraints.WEST;
+		gbc_lblVowels.gridwidth = 2;
+		gbc_lblVowels.insets = new Insets(0, 0, 5, 0);
+		gbc_lblVowels.gridx = 0;
+		gbc_lblVowels.gridy = 3;
+		panel.add(lblVowels, gbc_lblVowels);
+		
+		txtVowels = new JTextField();
+		txtVowels.setText("separated by spaces");
+		GridBagConstraints gbc_txtVowels = new GridBagConstraints();
+		gbc_txtVowels.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtVowels.gridwidth = 2;
+		gbc_txtVowels.insets = new Insets(0, 0, 5, 0);
+		gbc_txtVowels.gridx = 0;
+		gbc_txtVowels.gridy = 4;
+		panel.add(txtVowels, gbc_txtVowels);
+		txtVowels.setColumns(10);
+		
+		JLabel lblMaxsyl = new JLabel("Maximum syllables");
+		GridBagConstraints gbc_lblMaxsyl = new GridBagConstraints();
+		gbc_lblMaxsyl.anchor = GridBagConstraints.WEST;
+		gbc_lblMaxsyl.insets = new Insets(0, 0, 5, 5);
+		gbc_lblMaxsyl.gridx = 0;
+		gbc_lblMaxsyl.gridy = 5;
+		panel.add(lblMaxsyl, gbc_lblMaxsyl);
+		
+		txtMaxsyl = new JTextField();
+		GridBagConstraints gbc_txtMaxsyl = new GridBagConstraints();
+		gbc_txtMaxsyl.insets = new Insets(0, 0, 5, 0);
+		gbc_txtMaxsyl.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMaxsyl.gridx = 1;
+		gbc_txtMaxsyl.gridy = 5;
+		panel.add(txtMaxsyl, gbc_txtMaxsyl);
+		txtMaxsyl.setColumns(10);
+		
+		JLabel lblOto = new JLabel("OTO");
+		GridBagConstraints gbc_lblOto = new GridBagConstraints();
+		gbc_lblOto.insets = new Insets(0, 0, 5, 0);
+		gbc_lblOto.gridwidth = 2;
+		gbc_lblOto.anchor = GridBagConstraints.NORTH;
+		gbc_lblOto.gridx = 0;
+		gbc_lblOto.gridy = 6;
+		panel.add(lblOto, gbc_lblOto);
+		
+		JLabel lblInitoff = new JLabel("Initial offset");
+		GridBagConstraints gbc_lblInitoff = new GridBagConstraints();
+		gbc_lblInitoff.anchor = GridBagConstraints.WEST;
+		gbc_lblInitoff.insets = new Insets(0, 0, 5, 5);
+		gbc_lblInitoff.gridx = 0;
+		gbc_lblInitoff.gridy = 7;
+		panel.add(lblInitoff, gbc_lblInitoff);
+		
+		txtInitoff = new JTextField();
+		GridBagConstraints gbc_txtInitoff = new GridBagConstraints();
+		gbc_txtInitoff.insets = new Insets(0, 0, 5, 0);
+		gbc_txtInitoff.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtInitoff.gridx = 1;
+		gbc_txtInitoff.gridy = 7;
+		panel.add(txtInitoff, gbc_txtInitoff);
+		txtInitoff.setColumns(10);
+		
+		JLabel lblTempo = new JLabel("Tempo");
+		GridBagConstraints gbc_lblTempo = new GridBagConstraints();
+		gbc_lblTempo.anchor = GridBagConstraints.WEST;
+		gbc_lblTempo.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTempo.gridx = 0;
+		gbc_lblTempo.gridy = 8;
+		panel.add(lblTempo, gbc_lblTempo);
+		
+		txtTempo = new JTextField();
+		GridBagConstraints gbc_txtTempo = new GridBagConstraints();
+		gbc_txtTempo.insets = new Insets(0, 0, 5, 0);
+		gbc_txtTempo.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtTempo.gridx = 1;
+		gbc_txtTempo.gridy = 8;
+		panel.add(txtTempo, gbc_txtTempo);
+		txtTempo.setColumns(10);
+		
+		JLabel lblMaxdups = new JLabel("Maximum duplicates");
+		GridBagConstraints gbc_lblMaxdups = new GridBagConstraints();
+		gbc_lblMaxdups.anchor = GridBagConstraints.WEST;
+		gbc_lblMaxdups.insets = new Insets(0, 0, 5, 5);
+		gbc_lblMaxdups.gridx = 0;
+		gbc_lblMaxdups.gridy = 9;
+		panel.add(lblMaxdups, gbc_lblMaxdups);
+		
+		txtMaxdups = new JTextField();
+		GridBagConstraints gbc_txtMaxdups = new GridBagConstraints();
+		gbc_txtMaxdups.insets = new Insets(0, 0, 5, 0);
+		gbc_txtMaxdups.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMaxdups.gridx = 1;
+		gbc_txtMaxdups.gridy = 9;
+		panel.add(txtMaxdups, gbc_txtMaxdups);
+		txtMaxdups.setColumns(10);
+		
+		btnGenerate = new JButton("Generate");
+		GridBagConstraints gbc_btnGenerate = new GridBagConstraints();
+		gbc_btnGenerate.gridwidth = 2;
+		gbc_btnGenerate.insets = new Insets(0, 0, 0, 5);
+		gbc_btnGenerate.gridx = 0;
+		gbc_btnGenerate.gridy = 10;
+		panel.add(btnGenerate, gbc_btnGenerate);
+		
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		btnGenerate.addActionListener(this);
+	}
+	public void actionPerformed(ActionEvent e) { 
+		if (e.getSource() == btnGenerate){
+			String cons = txtConsonants.getText();
+			String vows = txtVowels.getText();
+			int syls = Integer.parseInt(txtMaxsyl.getText());
+			int off = Integer.parseInt(txtInitoff.getText());
+			int tempo = Integer.parseInt(txtTempo.getText());
+			int dups = Integer.parseInt(txtMaxdups.getText());
+			
+			VCVGenerator.reclist(cons,vows,syls);
+			VCVGenerator.oto(off, tempo, dups);
+			
+			JOptionPane.showMessageDialog(null, "Done.", "VCV Generator", JOptionPane.INFORMATION_MESSAGE);
+		 	frame.dispose();
+		}
+	}
+}
---
+++ src/vcvgen/VCVGenerator.java
@@ -0,0 +1,200 @@
+package vcvgen;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class VCVGenerator {
+	static ArrayList<String[]> reclist;
+	static int numOfVVs;
+	
+	public static void reclist(String cons, String vows, int maxSyl){
+		String[] consonants = cons.split(" ");
+		String[] vowels = vows.split(" ");
+		String[][] initVowelStrings = new String[vowels.length][vowels.length+1];
+		
+		for (int i = 0; i < vowels.length; i++){
+			String[] vv = new String[vowels.length+1];
+			vv[0] = vowels[i];
+			for (int j = 1; j < vowels.length+1; j++){
+				if (j%2 == 0){
+					vv[j] = vowels[(i+j/2)%vowels.length];
+				}else{
+					vv[j] = vowels[i];
+				}
+			}
+			initVowelStrings[i] = vv;
+		}
+		
+		ArrayList<String[]> finVowelStrings = new ArrayList<String[]>();
+		for (String[] line : initVowelStrings){
+			int start = 0;
+			int end = maxSyl;
+			if (line.length < maxSyl){
+				finVowelStrings.add(line);
+			} else {
+				boolean first = true;
+				while (end <= line.length){
+					
+					if (first){
+						String[] firstBatch = new String[maxSyl];
+						for (int i = 0; i < end; i++){
+							firstBatch[i] = line[i];
+						}
+						finVowelStrings.add(firstBatch);
+						start = end-1;
+						end = end + maxSyl-1;
+						first = false;
+					}
+					
+					if (line.length-start > maxSyl){
+						String[] midBatch = new String[maxSyl];
+						for (int i = start, j = 0; i < end; i++, j++){
+							midBatch[j] = line[i];
+						}
+						finVowelStrings.add(midBatch);
+						start = end-1;
+						end = end + maxSyl-1;
+					} else {
+						String[] finBatch = new String[line.length-start];
+						for (int i = start, j = 0; i < line.length; i++, j++){
+							finBatch[j] = line[i];
+						}
+						finVowelStrings.add(finBatch);
+						break;
+					}
+				}
+			}
+		}
+		
+		numOfVVs = finVowelStrings.size();
+		
+		reclist = new ArrayList<String[]>();
+		for (String[] line : finVowelStrings){
+			reclist.add(line);
+		}
+		for (String con : consonants){
+			for (String[] line : finVowelStrings){
+				String[] vcv = new String[line.length*2];
+				for (int i = 0; i < line.length; i++){
+					vcv[i*2] = con;
+					vcv[i*2+1] = line[i];
+				}
+				reclist.add(vcv);
+			}
+		}
+		
+		try {
+            PrintWriter writer = new PrintWriter(new File("./result/reclist.txt"));
+            
+            for (int i = 0; i < reclist.size(); i++){
+            	writer.println(getFilename(i));
+    		}
+            
+            writer.close();
+         } catch (Exception ex) {
+        	 ex.printStackTrace();
+         }
+	}
+	
+	public static void oto(int initOff, int tempo, int maxDups){
+		ArrayList<String[]> oto = new ArrayList<String[]>();
+		String cons = Integer.toString((int)((10000/tempo)*4.5));
+		String cutoff = Integer.toString(0-(10000/tempo)*7);
+		String preutt = Integer.toString((10000/tempo)*3);
+		String overlap= Integer.toString(10000/tempo);
+		int offInc = (10000/tempo)*6;
+		
+		for (int i = 0; i < numOfVVs; i++){
+			int currentOff = initOff;
+			String[] recArr = reclist.get(i);
+			String recLine = getFilename(i);
+			
+			String[] firstV = {recLine,"alias","offset",cons,cutoff,preutt,overlap};
+			String alias1 = "- " + recArr[0];
+			firstV[1] = alias1;
+			firstV[2] = Integer.toString(currentOff);
+			oto.add(firstV);
+			currentOff += offInc;
+			
+			for (int j = 0; j < recArr.length-1; j++){
+				String[] otoLine = {recLine,"alias","offset",cons,cutoff,preutt,overlap};
+				String alias = recArr[j] + " " + recArr[j+1];
+				otoLine[1] = alias;
+				otoLine[2] = Integer.toString(currentOff);
+				oto.add(otoLine);
+				currentOff += offInc;
+			}
+		}
+		for (int i = numOfVVs; i<reclist.size(); i++){
+			int currentOff = initOff;
+			String[] recArr = reclist.get(i);
+			String recLine = getFilename(i);
+			
+			String[] firstCV = {recLine,"alias","offset",cons,cutoff,preutt,overlap};
+			String alias1 = "- "+ recArr[0] + recArr[1];
+			firstCV[1] = alias1;
+			firstCV[2] = Integer.toString(currentOff);
+			oto.add(firstCV);
+			currentOff += offInc;
+			
+			for (int j = 2; j < recArr.length-1; j+=2){
+				String[] otoLine = {recLine,"alias","offset",cons,cutoff,preutt,overlap};
+				String alias = recArr[j-1] + " " + recArr[j] + recArr[j+1];
+				otoLine[1] = alias;
+				otoLine[2] = Integer.toString(currentOff);
+				oto.add(otoLine);
+				currentOff += offInc;
+			}
+		}
+		
+		try {
+			HashMap<String,Integer> aliasCount = new HashMap<String,Integer>();
+			PrintWriter writer = new PrintWriter(new File("./result/oto.ini"));
+			for (String[] line : oto){
+				if (aliasCount.containsKey(line[1]) && aliasCount.get(line[1]) < maxDups){
+					aliasCount.replace(line[1],aliasCount.get(line[1])+1);
+					line[1] += aliasCount.get(line[1]);
+					writer.println(line[0]+".wav="+line[1]+","+line[2]+","+line[3]+","+line[4]+","+line[5]+","+line[6]);
+				} else if (! aliasCount.containsKey(line[1])) {
+					aliasCount.put(line[1],0);
+					writer.println(line[0]+".wav="+line[1]+","+line[2]+","+line[3]+","+line[4]+","+line[5]+","+line[6]);
+				}
+			}
+			writer.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	private static String getFilename(int index){
+		String fn = "";
+		String[] line = reclist.get(index);
+		
+		if (index < numOfVVs){
+    		for (int i = 0; i < line.length; i++){
+				String ch = line[i];
+				if (i == line.length-1){
+					fn += ch;
+				} else {
+					fn += ch + "-";
+				}
+			}
+    	} else {
+    		int cvCounter = 0;
+			for (int i = 0; i < line.length; i++){
+				String ch = line[i];
+				if (cvCounter == 0 || i == line.length-1){
+					fn += ch;
+					cvCounter++;
+				} else {
+					fn += ch + "-";
+					cvCounter = 0;
+				}
+			}
+    	}
+		
+		return fn;
+	}
+}
